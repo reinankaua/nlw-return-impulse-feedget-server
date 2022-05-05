d22bc88abf3d8ed81253b5ca43fb034f9a2f2704
Reinan Kauã <contato.reinankaua@gmail.com>
2022-05-05 00:57:36 -0300

    server

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "3dcfac47aba547",
+        pass: "b043fa366f3779"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Reinan Kauã <contato.reinankaua@gmail.com>',
+            subject,
+            html: body,
+        });
+    }
+}
