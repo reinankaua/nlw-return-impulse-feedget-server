bfac6a61d7b48cf76cdab94da61d62bde6d062e6
Reinan Kauã <contato.reinankaua@gmail.com>
2022-05-06 15:29:02 -0300

    Update tables

--- src/routes.ts
+++ src/routes.ts
@@ -9,22 +9,31 @@ export const routes = express.Router();
 
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
 
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    
+        const nodemailerMailAdapter = new NodemailerMailAdapter();
+    
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        );
+    
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot,
+        });
+    
+    
+    
+        return res.status(201).send();
+        
+    } catch (error) {
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    );
-
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot,
-    });
-
-
-
-    return res.status(201).send();
+        console.log(error);
+        return res.status(500).send();
+        
+    }
 });
